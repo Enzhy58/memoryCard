d255cb0ab2027560ece982bfe286f0ed4d04bdf6
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-28 20:17:49 +0300

    added date attributes

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 function flipCard() {
     this.classList.toggle('flip');
+
+    if(!hasFlippedCard) {
+        //first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        //second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        console.log(firstCard.dataset.card);
+        console.log(secondCard.dataset.card);
+    }
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard));
