3435fd964d4fcf1b39c802bf9d1108d98ab6917b
Enzhy58 <ang.borisova2015@yandex.ru>
2024-02-29 12:10:37 +0300

    added shuffling of cards

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.toggle('flip');
 
     if (!hasFlippedCard) {
-        //first click
         hasFlippedCard = true;
         firstCard = this;
-    } else {
-        //second click
-        hasFlippedCard = false;
-        secondCard = this;
 
-        checkForMatch();
+        return;
     }
+
+    secondCard = this;
+
+    checkForMatch();
+
 }
 
 function checkForMatch() {
@@ -27,13 +31,31 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 function unflipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
 }
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
 
 cards.forEach(card => card.addEventListener('click', flipCard));
